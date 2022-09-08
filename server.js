7bddca920d1e57029576c1ec72b8e6307c739f47
Sioutis Jim <mitsosgr96@gmail.com>
2022-09-08 21:15:43 +0300

    make it better

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://diana-reactjs.herokuapp.com"],
     methods: ["GET", "POST", "DELETE","PUT"],
     credentials: true
 }))
